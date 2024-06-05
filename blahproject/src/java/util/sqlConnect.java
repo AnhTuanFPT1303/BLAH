063f124289d4ae6c3d202a27efb926331ff4f1e5
AnhTuanFPT1303 <155722044+AnhTuanFPT1303@users.noreply.github.com>
2024-06-05 13:42:26 +0700

    Login feature (#4)
    Co-authored-by: HELLO <HELLO@LAPTOP-5D2CNVK4>

--- blahproject/src/java/util/sqlConnect.java
+++ blahproject/src/java/util/sqlConnect.java
@@ -1,5 +1,6 @@
 package util;
 
+import dao.userDAO;
 import java.io.IOException;
 import java.sql.*;
 
@@ -13,16 +14,18 @@ import java.sql.*;
  */
 public class sqlConnect {
 
-    public static Connection javaconnectsqlserver() throws Exception {
-        Connection connection = null;
-        String userName = "sa";
-        String passWord = "123";
-        String port = "1433";
-        String ip = "127.0.0.1";
-        String dbName = "Class";
-        String deviceName = "LAPTOP-5D2CNVK4";
-        String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        String sqlUrl = "jdbc:sqlserver://LAPTOP-5D2CNVK4;databaseName=Class;encrypt=false;trustServerCertificate=false";
+    private static sqlConnect instance = null;
+    private Connection connection = null;
+    private String userName = "sa";
+    private String passWord = "123";
+    private String port = "1433";
+    private String ip = "127.0.0.1";
+    private String dbName = "Blah";
+    private String deviceName = "LAPTOP-5D2CNVK4";
+    private String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private String sqlUrl = "jdbc:sqlserver://LAPTOP-5D2CNVK4;databaseName=Blah;encrypt=false;trustServerCertificate=false";
+
+    public Connection javaconnectsql() throws Exception {
         try {
             Class.forName(driverClass);
             connection = DriverManager.getConnection(sqlUrl, userName, passWord);
@@ -30,5 +33,21 @@ public class sqlConnect {
             System.out.println("Connect Failed");
         }
         return connection;
+    }
+
+    public static sqlConnect getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new sqlConnect();
+        } 
+        return instance;
+    }
+
+    
+    public static void main(String[] args) throws SQLException {
+        userDAO userDao= new userDAO();
+        boolean test =userDao.login("admin123@gmail.com", "admin");
+        if(test) {
+            System.out.println("Success");
+        }
     }
 }
